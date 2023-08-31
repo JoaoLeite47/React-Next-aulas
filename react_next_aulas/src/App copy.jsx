3fa7f750daad2ed1141bab153775b84df70c5a78
JOAO LEITE <stalkerjj@outlook.com>
2023-08-31 15:47:49 -0300

    useRef

---
+++ react_next_aulas/src/App copy.jsx
@@ -0,0 +1,80 @@
+import P from 'prop-types';
+import { useEffect, useMemo, useState, useRef } from 'react';
+import './App.css';
+
+const Post = ({ post, handleClick }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
+  handleClick: P.func,
+};
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(0);
+
+  console.log('Pai renderizou!');
+
+  // Component did mount
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
+  }, []);
+
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
+
+  useEffect(() => {
+    contador.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
+
+  return (
+    <div className="App">
+      <h6>Renderizou: {contador.current}x</h6>
+      <p>
+        <input
+          ref={input}
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} handleClick={handleClick} />;
+          })
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
+    </div>
+  );
+}
+
+export default App;
+
+//useRef cria uma referencia ao elemento selecionado com o atributo ref={}, nisso podemos manipular o elemento, no caso, nos podemos colocar um foco no input com o código:   input.current.focus();
