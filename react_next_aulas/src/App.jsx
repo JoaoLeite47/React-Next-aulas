79689a1aa30e4794f8680dc95e921fd61b93112c
JOAO LEITE <stalkerjj@outlook.com>
2023-08-31 15:36:24 -0300

    useMemo exemplos

--- react_next_aulas/src/App.jsx
+++ react_next_aulas/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import P from "prop-types";
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
+
+const Post = ({ post }) => {
+  console.log("Filho renderizou");
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState("");
+
+  console.log("Pai renderizou!");
+
+  // Component did mount
+  useEffect(() => {
+    setTimeout(function () {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="App">
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
       </p>
-    </>
-  )
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })
+        ); // evita a renderização duplicada do componente no caso do input ser manipulado
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
